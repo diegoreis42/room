df9009551d7b156fbd222034aa7602f0943b2de8
Diego Reis <reisdiego144@gmail.com>
2024-10-10 10:14:43 -0300

    feat: adding intro banner but this affects the ascii_processing :(

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,13 @@
 mod ascii_processor;
+mod view;
 
-use std::{thread::sleep, time};
+use std::io;
 
-use ascii_processor::AsciiProcessor;
-use opencv::videoio::{self, VideoCaptureTrait};
+use view::View;
 
-fn main() {
-    // Set up video capture
-    let mut cam =
-        videoio::VideoCapture::new(0, videoio::CAP_ANY).expect("Failed to get video capture");
-
-    // Initialize ASCII processor with desired target width/height
-    let mut ascii_processor = AsciiProcessor::new(200, 90);
-
-    // Infinite loop for frame capture and processing
-    loop {
-        // Capture a frame from the webcam
-        cam.read(&mut ascii_processor.frame)
-            .expect("Failed to capture frame");
-
-        // Process and print the frame as ASCII art
-        ascii_processor.process();
-
-        // Adjust frame delay to balance speed (adjust as necessary)
-        let frame_delay = time::Duration::from_millis(33); // Roughly 30 FPS
-        sleep(frame_delay);
-    }
+fn main() -> io::Result<()> {
+    let mut terminal = ratatui::init();
+    let app_result = View::default().run(&mut terminal);
+    ratatui::restore();
+    app_result
 }
